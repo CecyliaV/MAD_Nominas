1829d374ca953eb5ff9c82c73ed797a882594aa6
Emiliano Leal <72473356+EmilianoLeal@users.noreply.github.com>
2020-11-29 03:20:27 -0600

    Empleado WIP

--- Extras/main/main/05. Departamento.vb
+++ Extras/main/main/05. Departamento.vb
@@ -1,21 +1,5 @@
 ﻿Public Class Form5
-    Public Temp As EnlaceBD
-
     Private Sub Form5_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
-    End Sub
-
-    Private Sub TextBox5_TextChanged(sender As Object, e As EventArgs) Handles TextBoxND.TextChanged
-
-    End Sub
-
-    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
-        Temp = New EnlaceBD
-        Dim data As New DataTable
-
-
-        Temp.AddDepartamento("I", TextBoxND.Text, TextBoxSBD.Text, Nothing)
-
 
     End Sub
 End Class
